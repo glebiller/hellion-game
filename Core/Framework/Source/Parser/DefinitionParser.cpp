82c4f733161f75cf5422acea4ceb09b863c89d95
Guillaume Le Biller <guillaume.lebiller@gmail.com>
2012-08-09 00:25:18 +0200

    Added proto generated files

---
+++ Core/Framework/Source/Parser/DefinitionParser.cpp
@@ -0,0 +1,301 @@
+﻿// Copyright � 2008-2009 Intel Corporation
+// All Rights Reserved
+//
+// Permission is granted to use, copy, distribute and prepare derivative works of this
+// software for any purpose and without fee, provided, that the above copyright notice
+// and this statement appear in all copies.  Intel makes no representations about the
+// suitability of this software for any purpose.  THIS SOFTWARE IS PROVIDED "AS IS."
+// INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, AND ALL LIABILITY,
+// INCLUDING CONSEQUENTIAL AND OTHER INDIRECT DAMAGES, FOR THE USE OF THIS SOFTWARE,
+// INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PROPRIETARY RIGHTS, AND INCLUDING THE
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  Intel does not
+// assume any responsibility for any errors which may appear in this software nor any
+// responsibility to update it.
+
+#include "DefinitionParser.h"
+
+/**
+ * @inheritDoc
+ */
+DefinitionParser::DefinitionParser(UScene* pScene, std::string sGDF)
+    : m_pScene(pScene) {
+    Error result = Singletons::PlatformManager.FileSystem().LoadProto(sGDF.c_str(), &m_gdProto);
+    ASSERT(result == Errors::Success);
+}
+
+/**
+ * @inheritDoc
+ */
+void DefinitionParser::ParseEnvironment(void) {
+    ASSERT(m_gdProto.IsInitialized());
+
+    const ProtoPropertyList& properties = m_gdProto.properties();
+    for (ProtoPropertyList::const_iterator propertiesIt = properties.begin(); propertiesIt != properties.end(); propertiesIt++) {
+        // Environment properties only have one value
+        Singletons::EnvironmentManager.Variables().Add(propertiesIt->name().c_str(), propertiesIt->value().Get(0).c_str());
+    }
+}
+
+/**
+ * @inheritDoc
+ */
+void DefinitionParser::ParseSystems(void) {
+    ASSERT(m_gdProto.systems_size() > 0);
+
+    const ProtoSystemList& systems = m_gdProto.systems();
+    for (ProtoSystemList::const_iterator systemsIt = systems.begin(); systemsIt != systems.end(); systemsIt++) {
+        Singletons::PlatformManager.FileSystem().LoadSystemLibrary(systemsIt->lib().c_str(), &m_pSystem);
+        // Get the default properties from system, then Initialize it
+        m_pSystem->GetProperties(m_GetProperties);
+        ApplyProperties(static_cast<ProtoPropertyList>(systemsIt->properties()), m_GetProperties, m_SetProperties);
+        m_pSystem->Initialize(m_SetProperties);
+        m_GetProperties.clear();
+        m_SetProperties.clear();
+        ASSERTMSG1(strcmp(systemsIt->type().c_str(), m_pSystem->GetName()) == 0,
+                   "Parser identified an incorrect system type. It should be %s.", m_pSystem->GetName());
+    }
+}
+
+/**
+ * @inheritDoc
+ */
+void DefinitionParser::ParseScene(std::string sScene) {
+    const ProtoSceneList& scenes = m_gdProto.scenes();
+    for (ProtoSceneList::const_iterator scenesIt = scenes.begin(); scenesIt != scenes.end(); scenesIt++) {
+        if (scenesIt->it .compare(sScene) == 0) {
+            //
+            // Create the initial scene for each system.
+            //
+            ISystem* pSystem = Singletons::SystemManager.GetFirst();
+
+            while (pSystem != NULL) {
+                m_pScene->Extend(pSystem);
+                pSystem = Singletons::SystemManager.GetNext();
+            }
+
+            //
+            // Parse the SDF file
+            //
+            Proto::Sdf sdf;
+            std::string sceneProtoFile = scenesIt->sdf() + ".sdf.bin";
+            Error result = Singletons::PlatformManager.FileSystem().LoadProto(sceneProtoFile.c_str(), &sdf);
+            ASSERT(result == Errors::Success);
+
+            //
+            // Initialize the System scenes.
+            //
+            for (ProtoSystemPropertiesList::const_iterator systemPropertiesIt = sdf.systemproperties().begin();
+                    systemPropertiesIt != sdf.systemproperties().end();
+                    systemPropertiesIt++) {
+                m_pSystem = Singletons::SystemManager.Get(systemPropertiesIt->systemtype().c_str());
+                ASSERTMSG1(m_pSystem != NULL,
+                           "Parser was unable to get system %s.", systemPropertiesIt->systemtype().c_str());
+
+                if (m_pSystem != NULL) {
+                    UScene::SystemScenesConstIt it = m_pScene->GetSystemScenes().find(m_pSystem->GetSystemType());
+                    ASSERTMSG1(it != m_pScene->GetSystemScenes().end(),
+                               "Parser was unable to find a scene for system %s.", systemPropertiesIt->systemtype().c_str());
+                    m_pSystemScene = it->second;
+                    ASSERT(m_pSystemScene != NULL);
+                    // Initialize system scene properties
+                    m_pSystemScene->GetProperties(m_GetProperties);
+                    ApplyProperties(static_cast<ProtoPropertyList>(systemPropertiesIt->properties()), m_GetProperties, m_SetProperties);
+                    m_pSystemScene->Initialize(m_SetProperties);
+                    m_GetProperties.clear();
+                    m_SetProperties.clear();
+                }
+            }
+
+            //
+            // Initialize the scene objects.
+            //
+            for (ProtoObjectList::const_iterator objectsIt = sdf.objects().begin();
+                    objectsIt != sdf.objects().end();
+                    objectsIt++) {
+                //
+                // Create the object.
+                //
+                m_pUObject = m_pScene->CreateObject();
+                ASSERT(m_pUObject != NULL);
+                m_pUObject->SetName(objectsIt->name().c_str());
+
+                //
+                // Added systems extension.
+                //
+                for (ProtoObjectPropertiesList::const_iterator objectPropertiesIt = objectsIt->objectproperties().begin();
+                        objectPropertiesIt != objectsIt->objectproperties().end();
+                        objectPropertiesIt++) {
+                    m_pSystem = Singletons::SystemManager.Get(objectPropertiesIt->systemtype().c_str());
+                    ASSERTMSG1(m_pSystem != NULL, "Parser was unable to get system %s.", objectPropertiesIt->systemtype().c_str());
+
+                    if (m_pSystem != NULL) {
+                        UScene::SystemScenesConstIt it = m_pScene->GetSystemScenes().find(m_pSystem->GetSystemType());
+                        ASSERTMSG1(it != m_pScene->GetSystemScenes().end(),
+                                   "Parser was unable to find a scene for the system %s.", m_pSystem->GetSystemType());
+                        //
+                        // Create object.
+                        //
+                        m_pSystemObject = m_pUObject->Extend(it->second, objectPropertiesIt->objecttype().c_str());
+                        ASSERT(m_pSystemObject != NULL);
+
+                        if (m_pSystemObject != NULL) {
+                            //
+                            // Initialize the extension.
+                            //
+                            m_pSystemObject->GetProperties(m_GetProperties);
+                            ApplyProperties(static_cast<ProtoPropertyList>(objectPropertiesIt->properties()), m_GetProperties, m_SetProperties);
+                            m_pSystemObject->Initialize(m_SetProperties);
+                            m_GetProperties.clear();
+                            m_SetProperties.clear();
+                        }
+                    }
+                }
+            }
+
+            const UScene::SystemScenes Scenes = m_pScene->GetSystemScenes();
+
+            for (UScene::SystemScenesConstIt it = Scenes.begin(); it != Scenes.end(); it++) {
+                it->second->GlobalSceneStatusChanged(
+                    ISystemScene::GlobalSceneStatus::PostLoadingObjects
+                );
+            }
+
+            //
+            // Initialize the links.
+            //
+            for (ProtoLinksList::const_iterator linksIt = sdf.links().begin();
+                    linksIt != sdf.links().end();
+                    linksIt++) {
+                UObject* pSubject = m_pScene->FindObject(linksIt->subject().c_str());
+                UObject* pObserver = m_pScene->FindObject(linksIt->observer().c_str());;
+                std::string sSystemSubject = linksIt->subjectsystem();
+                std::string sSystemObserver = linksIt->observersystem();
+                ISystemObject* pSystemSubject = NULL;
+                ISystemObject* pSystemObserver = NULL;
+                ISystem* pSystem;
+
+                //
+                // Get the extension for the subject.
+                //
+                if (!sSystemSubject.empty()) {
+                    pSystem = Singletons::SystemManager.Get(sSystemSubject.c_str());
+                    pSystemSubject = pSubject->GetExtension(pSystem->GetSystemType());
+                }
+
+                //
+                // Get the extension for the object.
+                //
+                pSystem = Singletons::SystemManager.Get(sSystemObserver.c_str());
+                pSystemObserver = pObserver->GetExtension(pSystem->GetSystemType());
+
+                //
+                // Call the scene to register the links.
+                //
+                if (pSystemSubject != NULL) {
+                    m_pScene->CreateObjectLink(pSystemSubject, pSystemObserver);
+                } else {
+                    m_pScene->CreateObjectLink(pSubject, pSystemObserver);
+                }
+            }
+
+            break;
+        }
+    }
+}
+
+std::string
+DefinitionParser::StartupScene(
+    void
+) {
+    return m_Gdf.startupscene();
+}
+
+void
+DefinitionParser::ApplyProperties(
+    ProtoPropertyList& propertyList,
+    Properties::Array& getProperties,
+    Properties::Array& setProperties
+) const {
+    Properties::ConstIterator getPropIt = getProperties.end();
+    size_t iProp = 0xFFFFFFFF;
+
+    for (ProtoPropertyList::const_iterator prop = propertyList.begin();
+            prop != propertyList.end();
+            prop++) {
+        //
+        // Search for the name in the property array.
+        //
+        for (Properties::ConstIterator it = getProperties.begin();
+                it != getProperties.end();
+                it++) {
+            //
+            // If the name of the property match, it's good
+            //
+            if (strcmp(it->GetName(), prop->name().c_str()) == 0) {
+                setProperties.push_back(*it);
+                iProp = setProperties.size() - 1;
+                getPropIt = it;
+                break;
+            }
+        }
+
+        ASSERTMSG1(getPropIt != getProperties.end(),
+                   "Parser could not find the property named %s in the list given by the system.", prop->name());
+        i32 iValue = 0;
+
+        for (ProtoStringList::const_iterator value = prop->value().begin();
+                value != prop->value().end();
+                value++) {
+            //
+            // Read in the property.
+            //
+            switch (getPropIt->GetValueType(iValue)) {
+                case Properties::Values::None:
+                    ASSERTMSG1(false, "Parser encoutered a value '%s' with no type.", prop->name());
+                    break;
+
+                case Properties::Values::Boolean:
+                case Properties::Values::Int32:
+                    setProperties[ iProp ].SetValue(iValue, Convertor::StringToInt(*value));
+                    break;
+
+                case Properties::Values::Enum: {
+                    for (i32 i = 0; getPropIt->GetEnumOption(i) != NULL; i++) {
+                        if (strcmp((*value).c_str(), getPropIt->GetEnumOption(i)) == 0) {
+                            setProperties[ iProp ].SetValue(iValue, i);
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+
+                case Properties::Values::Float32:
+                case Properties::Values::Vector3& Properties::Values::Mask:
+                case Properties::Values::Vector4& Properties::Values::Mask:
+                case Properties::Values::Quaternion& Properties::Values::Mask:
+                case Properties::Values::Color3& Properties::Values::Mask:
+                case Properties::Values::Color4& Properties::Values::Mask:
+                    setProperties[ iProp ].SetValue(iValue, Convertor::StringToFloat(*value));
+                    break;
+
+                case Properties::Values::Angle:
+                    setProperties[ iProp ].SetValue(
+                        iValue, Math::Angle::Deg2Rad(Convertor::StringToFloat(*value))
+                    );
+                    break;
+
+                case Properties::Values::String:
+                case Properties::Values::Path:
+                    setProperties[ iProp ].SetValue(iValue, *value);
+                    break;
+
+                default:
+                    ASSERTMSG(false, "Parser encountered an unsupported property value.");
+                    break;
+            }
+
+            iValue++;
+        }
+    }
+}
---
+++ Core/Framework/Source/Parser/DefinitionParser.h
@@ -0,0 +1,99 @@
+﻿// Copyright � 2008-2009 Intel Corporation
+// All Rights Reserved
+//
+// Permission is granted to use, copy, distribute and prepare derivative works of this
+// software for any purpose and without fee, provided, that the above copyright notice
+// and this statement appear in all copies.  Intel makes no representations about the
+// suitability of this software for any purpose.  THIS SOFTWARE IS PROVIDED "AS IS."
+// INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, AND ALL LIABILITY,
+// INCLUDING CONSEQUENTIAL AND OTHER INDIRECT DAMAGES, FOR THE USE OF THIS SOFTWARE,
+// INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PROPRIETARY RIGHTS, AND INCLUDING THE
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  Intel does not
+// assume any responsibility for any errors which may appear in this software nor any
+// responsibility to update it.
+
+#pragma once
+
+#include <string>
+
+#include "Assert.h"
+#include "Proto.h"
+#include "Universal.h"
+#include "System\ISystem.h"
+#include "System\ISystemObject.h"
+
+/**
+ * Gdf parser.
+ */
+class DefinitionParser {
+    public:
+
+        /**
+         * Constructor.
+         *
+         * @param [in,out]  pScene  If non-null, the scene.
+         * @param   sGDF            The gdf.
+         */
+        DefinitionParser(UScene* pScene, std::string sGDF);
+
+        /**
+         * Parse environment.
+         */
+        void ParseEnvironment(void);
+
+        /**
+         * Parse systems.
+         */
+        void ParseSystems(void);
+
+        /**
+         * Parse scene.
+         *
+         * @param   sScene  The scene.
+         */
+        void ParseScene(std::string sScene);
+
+        /**
+         * Gets the startup scene.
+         *
+         * @return The startup scene.
+         */
+        std::string StartupScene(void);
+
+        /**
+         * Assignment operator.
+         *
+         * @param   parameter1  The first parameter.
+         * @return  A shallow copy of this DefinitionParser.
+         */
+        DefinitionParser& operator=(const DefinitionParser&) {
+            ASSERT(false);
+        }
+
+    protected:
+
+        void ApplyProperties(
+            ProtoPropertyList& PropertyList,
+            Properties::Array& GetProperties,
+            Properties::Array& SetProperties
+        ) const;
+
+
+    protected:
+
+        GlobalDefinitionProto                           m_gdProto;
+
+        UScene*                                         m_pScene;
+        ISystem*                                        m_pSystem;
+        ISystemScene*                                   m_pSystemScene;
+
+        UObject*                                        m_pUObject;
+        ISystemObject*                                  m_pSystemObject;
+
+        Properties::Array                               m_GetProperties;
+        Properties::Array                               m_SetProperties;
+
+        std::map<u32, Properties::Array>                 m_SetPropertiesMap;
+        std::map<UObject*, std::map<u32, Properties::Array>>  m_AllObjectProperties;
+
+};
