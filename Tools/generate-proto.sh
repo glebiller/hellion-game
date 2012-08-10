d6b3328f6927ba0c40134c5c5d0d84f18e6e9070
Guillaume Le Biller <guillaume.lebiller@gmail.com>
2012-08-10 17:38:08 +0200

    More on server

--- Tools/generate-proto.sh
+++ Tools/generate-proto.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-./protoc --proto_path=../Assets/Proto/ --cpp_out=../Core/BaseTypes/Source/Proto --java_out=./definition-encoder/src/main/java ../Assets/Proto/Common/*.proto ../Assets/Proto/Definition/*.proto
+protoc --proto_path=../Assets --cpp_out=../Core/BaseTypes/Source --java_out=./definition-encoder/src/main/java ../Assets/Proto/Common/*.proto ../Assets/Proto/Definition/*.proto
+protoc --proto_path=../Assets --cpp_out=../Core/BaseTypes/Source --java_out=../Main/HelionServer/src/main/java/ ../Assets/Proto/Server/*.proto
